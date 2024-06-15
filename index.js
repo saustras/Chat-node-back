ef7b93c67fd6a0be6bfd354ac292e962ee4822d5
federico <federendon26@hotmail.com>
2024-06-14 22:13:06 -0500

    feat:fix

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const router = require('./router/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
+// const app = express()
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
@@ -14,6 +15,12 @@ app.use(express.json())
 app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
+
+app.get('/',(request,response)=>{
+    response.json({
+        message : "Server running at " + PORT
+    })
+})
 
 //api endpoints
 app.use('/api',router)
